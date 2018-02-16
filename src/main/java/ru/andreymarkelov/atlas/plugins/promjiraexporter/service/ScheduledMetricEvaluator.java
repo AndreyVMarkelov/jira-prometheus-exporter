8bff014f2751716b67a3b754e033ae7d78cb609a
Wolfgang Brauneis <wolfgang.brauneis@e-mundo.de>
2018-02-16 14:41:18 +0100

    Configuration for attachment scraping interval

--- src/main/java/ru/andreymarkelov/atlas/plugins/promjiraexporter/service/ScheduledMetricEvaluator.java
+++ src/main/java/ru/andreymarkelov/atlas/plugins/promjiraexporter/service/ScheduledMetricEvaluator.java
@@ -2,4 +2,12 @@ package ru.andreymarkelov.atlas.plugins.promjiraexporter.service;
 
 public interface ScheduledMetricEvaluator {
     long getTotalAttachmentSize();
+
+    long getLastExecutionTimestamp();
+
+    void restartScraping(int newDelay);
+
+    int getDelay();
+
+    void setDelay(int delay);
 }
