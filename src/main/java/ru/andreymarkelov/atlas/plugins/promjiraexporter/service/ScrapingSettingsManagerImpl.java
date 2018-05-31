77f4110f436b40aa8895618dc7b9811d82a0d96c
amarkelov <andrey.markelov@infobip.com>
2018-05-31 16:56:11 +0300

    Update version. Checked jira 7.10.0

---
+++ src/main/java/ru/andreymarkelov/atlas/plugins/promjiraexporter/service/ScrapingSettingsManagerImpl.java
@@ -0,0 +1,31 @@
+package ru.andreymarkelov.atlas.plugins.promjiraexporter.service;
+
+import com.atlassian.sal.api.pluginsettings.PluginSettings;
+import com.atlassian.sal.api.pluginsettings.PluginSettingsFactory;
+
+import static org.apache.commons.lang3.math.NumberUtils.toInt;
+
+public class ScrapingSettingsManagerImpl implements ScrapingSettingsManager {
+    private static final int DEFAULT_SCRAPE_DELAY = 1;
+
+    private final PluginSettings pluginSettings;
+
+    public ScrapingSettingsManagerImpl(PluginSettingsFactory pluginSettingsFactory) {
+        this.pluginSettings = pluginSettingsFactory.createSettingsForKey("PLUGIN_PROMETHEUS_FOR_JIRA");
+    }
+
+    @Override
+    public int getDelay() {
+        Object storedValue = getPluginSettings().get("delay");
+        return storedValue != null ? toInt(storedValue.toString(), DEFAULT_SCRAPE_DELAY) : DEFAULT_SCRAPE_DELAY;
+    }
+
+    @Override
+    public void setDelay(int delay) {
+        getPluginSettings().put("delay", String.valueOf(delay));
+    }
+
+    private synchronized PluginSettings getPluginSettings() {
+        return pluginSettings;
+    }
+}
